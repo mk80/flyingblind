bae11a9409c6aef681151c84209111dc01923e30
mk80 <mk80@users.noreply.github.com>
2016-07-20 20:12:51 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: michael
+ *
+ * Created on July 20, 2016, 8:07 PM
+ * 
+ * Creating control program for flight controls of a quad copter
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
