53242f7af68ba92d27281fb0ec061e628caa84eb
mk80 <michaelkosko@gmail.com>
2016-07-21 16:21:43 -0700

    second commit, starting populating class methods and fields

--- main.cpp
+++ main.cpp
@@ -8,14 +8,35 @@
  */
 
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
 /* Sensor class to maintain readings from sensors */
 class Sensor
 {
+    string placement;
     int value;
     
+public:
+    /* constructor */
+    void sensorConstruct(string facing)
+    {
+        placement = facing;
+        value = 0;
+    }
+    
+    /* methods */
+    void setPlacement(string facing)
+    {
+        placement = facing;
+    }
+    
+    int getValue()
+    {
+        /* this will need code to get value from analog sensors */
+        return value;
+    }
     
 };
 
@@ -23,8 +44,27 @@ class Sensor
 /* This will control movement of the copter          */
 class Controller
 {
+    string id;
     int forward, back, left, right, up, down;
     
+public:
+    /* constructor */
+    void controllerConstruct(string name)
+    {
+        id = name;
+        forward = back = left = right = up = down = 0;
+    }
+    
+    /* methods */
+    void setMovement(string movement, int amount)
+    {
+        /* this will need to determine which field to adjust */
+    }
+    
+    void motorControl(int num, int amount)
+    {
+        /* this will need to adjust motor rpm */
+    }
     
 };
 
